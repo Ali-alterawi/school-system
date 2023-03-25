6637a14c7d7e4fdee8511bfca03721d4aec2f601
Ali-alterawi <alielterawe@gmail.com>
2023-03-25 22:39:18 +0300

    edit bg-img

--- app.js
+++ app.js
@@ -3,31 +3,33 @@ let submittedInfo = document.getElementById("Submit-Info");
 
 studentInfo.addEventListener("submit", (e) => {
   e.preventDefault();
-  let stuName = e.target.fName.value;
+  let stName = e.target.fName.value;
   let date = e.target.birthday.value;
-  let male = e.target.gender.checked ? "female" : "male";
-  let female = e.target.gender.value;
+  let male = e.target.gender.value;
   let phone = e.target.grade.value;
   let grade = e.target.phoneNumber.value;
 
-   ali(stuName, date, male, phone, grade);
+
+  
+
+   ali(stName, date, male, phone, grade);
    });
 
-function ali(stuName, date, male, phone, grade) {
+function ali(stName, date, male, phone, grade) {
   let row = document.createElement("tr");
-  let pstuName = document.createElement("td");
+  let pstName = document.createElement("td");
   let pdate = document.createElement("td");
   let pgender = document.createElement("td");
   let pphone = document.createElement("td");
   let pgrade = document.createElement("td");
 
-  pstuName.textContent = stuName;
+  pstName.textContent = stName;
   pdate.textContent = date;
   pgender.textContent = male;
   pphone.textContent = phone;
   pgrade.textContent = grade;
 
-  row.appendChild(pstuName);
+  row.appendChild(pstName);
   row.appendChild(pdate);
   row.appendChild(pgender);
   row.appendChild(pphone);
